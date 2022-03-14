b829bb8c75ca49f82d560723c769bb55de70c266
pouyatirdad <pouyatirdad70@gmail.com>
2022-03-14 22:21:13 +0330

    add data and service layer and model in data

---
+++ Backend/Ticket.Api/Ticket.Data/Model/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket.Data.Model
+{
+    public class Ticket
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required, MaxLength(250)]
+        public string Title { get; set; }
+        [MaxLength(1000)]
+        public string Description { get; set; }
+        public int Status{ get; set; }
+        public bool isDeleted { get; set; }
+
+    }
+}
