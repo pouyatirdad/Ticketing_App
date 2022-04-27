74bbf1e58fda9006acd3a9704cb9f4ff6032dbf4
pouyatirdad <pouyatirdad70@gmail.com>
2022-04-27 22:55:40 +0430

    add account controller and that viewmodel

---
+++ Backend/Ticket.Api/Ticket.Data/ViewModel/Account/Register.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket.Data.ViewModel.Account
+{
+    public class Register
+    {
+        [Required,Display(Name ="نام کاربری")]
+        public string UserName { get; set; }
+        [Required, Display(Name = "ایمیل"),EmailAddress]
+        public string Email { get; set; }
+        [Required, Display(Name = "رمز عبور"),DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required, Display(Name = "تکرار رمز عبور"),Compare(nameof(Password)),DataType(DataType.Password)]
+        public string RetypePassword { get; set; }
+    }
+}
