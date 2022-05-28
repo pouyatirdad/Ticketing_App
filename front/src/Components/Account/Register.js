45f1e290744e1ce1727015e8eb60757d04a791b8
pouyatirdad <pouyatirdad70@gmail.com>
2022-05-28 20:23:39 +0430

    add register

--- front/src/Components/Account/Register.js
+++ front/src/Components/Account/Register.js
@@ -1,8 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Api from './../../Api/Api';
 
 function Register() {
+    const [State, setState] = useState({
+        data: {
+            username: "",
+            email: "",
+            password: "",
+            retypepassword: ""
+        },
+        error: false,
+    })
+
+    let MainSetState = data => {
+        setState({ ...State, ...data });
+    }
+
+    let logFunc = e => {
+        console.log(`error : ${e}`);
+        MainSetState({ data: "", error: true });
+    }
+
+
+    let submitRegister = e => {
+        e.preventDefault();
+
+        Api.post('/account/Register', {
+            username: State.data.username,
+            email: State.data.email,
+            password: State.data.password,
+            retypepassword: State.data.retypepassword
+        })
+            .then(x => MainSetState({ data: { username: "", email: "", password: "", retypepassword: "" }, error: false }))
+            .catch(err => logFunc(err));
+
+    }
     return (
-        <div>Register</div>
+        <div>
+
+            <h2>
+                Register
+            </h2>
+
+            {
+                State.error ? <p> We Got Error </p> : ""
+            }
+
+            <form onSubmit={submitRegister}>
+                <input placeholder="UserName"
+                    value={State.data.username} onChange={(e) => { MainSetState({ data: { ...State.data, username: e.target.value }, error: false }) }} />
+
+                <input placeholder="Email"
+                    value={State.data.email} onChange={(e) => { MainSetState({ data: { ...State.data, email: e.target.value }, error: false }) }} />
+
+                <input placeholder="Password"
+                    value={State.data.password} onChange={(e) => { MainSetState({ data: { ...State.data, password: e.target.value }, error: false }) }} />
+
+                <input placeholder="RetypePassword"
+                    value={State.data.retypepassword} onChange={(e) => { MainSetState({ data: { ...State.data, retypepassword: e.target.value }, error: false }) }} />
+
+                <button>
+                    Submit
+                </button>
+            </form>
+
+        </div>
     )
 }
 
--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './Components/App';
 import ConversationDetail from './Components/Conversation/ConversationDetail';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from './Components/Account/Register';
+import Login from './Components/Account/Login';
 
 ReactDOM.render(
 
@@ -13,6 +15,8 @@ ReactDOM.render(
       <Route path="Cvr" element={<ConversationDetail />}>
         <Route path=":id" element={<ConversationDetail />} />
       </Route>
+      <Route path='/register' exact element={<Register />} />
+      <Route path='/login' exact element={<Login />} />
     </Routes>
   </BrowserRouter>
   , document.getElementById('root')
