fe2fbb4436221dd1a6990a7eca50c1fd9426cab2
pouyatirdad <pouyatirdad70@gmail.com>
2022-06-11 21:53:25 +0430

    add xunit

---
+++ Backend/Ticket.Api/TicketTest/AccountTest.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using Ticket.Api.Controllers;
+using Xunit;
+
+namespace TicketTest
+{
+    public class AccountTest
+    {
+        private readonly AccountController accountController;
+        //private readonly Mock<Ilogin>
+
+        [Fact]
+        public void Login()
+        {
+
+        }
+    }
+}
