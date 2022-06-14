d4532c5c9c5757c82a3d150c4defa476fa4598e3
pouyatirdad <pouyatirdad70@gmail.com>
2022-06-14 22:14:00 +0430

    add ticket test

---
+++ Backend/Ticket.Api/TicketTest/TicketTest.cs
@@ -0,0 +1,26 @@
+﻿using Moq;
+using System.Collections.Generic;
+using Ticket.Api.Controllers;
+using Ticket.Service.Service;
+using Xunit;
+
+namespace TicketTest
+{
+    public class TicketTest
+    {
+        private readonly TicketController ticketController;
+        private readonly Mock<ITicketService> ticketService;
+        public TicketTest()
+        {
+            ticketService=new Mock<ITicketService>();
+            ticketController = new TicketController(ticketService.Object);
+        }
+        [Fact]
+        public void GetALl_True()
+        {
+            var result = (ticketController.Get() as IEnumerable<Ticket.Data.Model.Ticket>);
+            Assert.NotNull(result);
+            Assert.Equal("", result,ignoreCase:true);
+        }
+    }
+}
