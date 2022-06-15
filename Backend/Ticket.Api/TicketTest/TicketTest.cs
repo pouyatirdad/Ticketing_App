c60e0bbe2f690eba67de9bc08f3176541f58b3b4
pouya <66117587+pouyatirdad@users.noreply.github.com>
2022-06-15 09:08:33 +0430

    add mock.verify and result.count in ticket test

--- Backend/Ticket.Api/TicketTest/TicketTest.cs
+++ Backend/Ticket.Api/TicketTest/TicketTest.cs
@@ -20,6 +20,12 @@ namespace TicketTest
         {
             var result = (ticketController.Get() as IEnumerable<Ticket.Data.Model.Ticket>);
             Assert.NotNull(result);
+            
+            //this is for test, call the service in controller
+            //mockContext.Verify(x => x.SaveChanges(), Times.Once());
+            //mockContext.Verify(x => x.SaveChanges(), Times.Exactly(1));
+            
+            //add result.count()
             Assert.Equal("", result,ignoreCase:true);
         }
     }
